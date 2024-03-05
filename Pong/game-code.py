727c5fefb5b7554a80a73906bb2b24344146a3ad
Joshua <joshuawalker610@gmail.com>
2024-03-05 16:24:13 -0600

    Test Run

--- Pong/game-code.py
+++ Pong/game-code.py
@@ -127,3 +127,5 @@ while True:
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
         ball.setx(-340)
         ball.dx *= -1
+
+        #Test
