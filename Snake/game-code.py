28b06bc2c510850d1b02f50b7e6cb125eebd4e0a
Chigozie Onyia <51960769+gozie11@users.noreply.github.com>
2024-03-07 23:20:53 -0600

    added a bit of the skeletojcxnnvkjj jkjkj;n;kjj

---
+++ Snake/game-code.py
@@ -0,0 +1,17 @@
+class snake(object):
+    def __init__(self, color, pos) -> None:
+        pass
+
+    def move(self) -> None:
+        pass
+
+    def reset(self) -> None:
+        pass
+    def addCube(self) -> None:
+        pass
+
+    def draw(self, surface) -> None:
+        pass
+def drawGrid(w, rows, surface):
+    pass
+def redrawWindow(surface):d
