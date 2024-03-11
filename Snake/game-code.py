31b12ad334db8be7e2bda8369dd548d5c2249ce0
Gozie Onyia <gozieo7@gmail.com>
2024-03-11 03:52:25 -0500

    Add the cube class to the game

--- Snake/game-code.py
+++ Snake/game-code.py
@@ -1,3 +1,24 @@
+# It's a good idea to learn how to use pygame to create a game
+# https://www.pygame.org/wiki/about
+
+# The game is a simple snake game, where the snake has to eat the food and grow
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    row = 0
+    w = 0  
+    def __init__(self, start, dirnx=1, dirny=0, color=(255,0,0)) -> None:
+        pass
+    def move(self, dirnx, dirny) -> None:
+        pass
+    def draw(self, surface, eyes=False) -> None:
+        pass
+
 class snake(object):
     def __init__(self, color, pos) -> None:
         pass
@@ -5,7 +26,7 @@ class snake(object):
     def move(self) -> None:
         pass
 
-    def reset(self) -> None:
+    def reset(self, pos) -> None:
         pass
     def addCube(self) -> None:
         pass
